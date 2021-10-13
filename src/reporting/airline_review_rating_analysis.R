a900e2c7836aaaf31721b8c7aa7b3ef6ec86e311
Sam <sam.messaoudi@hotmail.nl>
2021-10-13 14:54:13 +0200

    final changes to the analysis

--- src/reporting/airline_review_rating_analysis.R
+++ src/reporting/airline_review_rating_analysis.R
@@ -7,10 +7,15 @@ KLM_data <- read_csv2('../../data/klm-royal-dutch-airlines_data_12-10-21.csv', n
 View(KLM_data)
 glimpse(KLM_data)
 
+table(KLM_data$`Verify Status`)
+
+# subset data to only look at verified reviews
+KLM_data_ver <- KLM_data %>% subset(KLM_data$`Verify Status` == 'Trip Verified') 
+
 # subsetting the data pre_covid (Date Flown = 2019) and post covid (Date Flown is 2020 or 2021)
 library(stringr)
-pre_covid <- subset(KLM_data, str_detect(KLM_data$`Date Flown`, '2018|2019') == TRUE)
-post_covid <- subset(KLM_data, str_detect(KLM_data$`Date Flown`, '2020|2021') == TRUE) 
+pre_covid <- subset(KLM_data_ver, str_detect(KLM_data_ver$`Date Flown`, '2018|2019') == TRUE)
+post_covid <- subset(KLM_data_ver, str_detect(KLM_data_ver$`Date Flown`, '2020|2021') == TRUE) 
 
 # average review rating pre and post covid
 average_rating_pre_covid <- mean(pre_covid$`Review Rating`)
@@ -18,4 +23,4 @@ average_rating_post_covid <- mean(post_covid$`Review Rating`)
 
 average_rating_post_covid-average_rating_pre_covid
 
-# Thus, the review ratings for KLM seem to have dropped by 1.4 after COVID-19
+# Thus, the review ratings for KLM seem to have dropped by around 1.2 after COVID-19
